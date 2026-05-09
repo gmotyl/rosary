782a179ca78ea94dd3449598cd0b1fde442aac03
Greg Motyl <gmotyl@gmail.com>
2026-05-09 18:05:05 +0200

    feat(intention): add optional completedRosaries counter to type

--- src/pages/IntentionPage/Interface.d.ts
+++ src/pages/IntentionPage/Interface.d.ts
@@ -2,8 +2,8 @@ import {MysteryTypes} from 'src/consts/MysteryTypes'
 
 export interface IIntention {
   id: string
-  userId?: string
   title: string
   description: string
   currentMystery: MysteryTypes
+  completedRosaries?: number
 }
